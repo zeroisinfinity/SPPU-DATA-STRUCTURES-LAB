942e53134e42e2c4e0a1a09e1aa297efcb2ba01f
Sahil More <skm842011@gmail.com>
2024-08-21 00:37:59 +0530

    Update and rename set.py to Python_Group_A_1.py

--- Python_Group_A_1.py
+++ Python_Group_A_1.py
@@ -1,3 +1,14 @@
+"""
+        Write a Python program to compute following operations on String: 
+        a) To display word with the longest length
+        b) To determines the frequency of occurrence of particular character in the string
+        c) To check whether given string is palindrome or not 
+        d) To display index of first appearance of the substring  
+        e) To count the occurrences of each word in a given string
+
+
+"""
+
 cricket = []
 football = []
 badminton = []
