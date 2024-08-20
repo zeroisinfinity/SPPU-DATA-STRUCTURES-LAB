6cd483e9a35769e16d8644ca462855a6afc23c84
zeroisinfinity <skm842011@gmail.com>
2024-08-21 00:33:37 +0530

    Python_Group_A_5.py

--- stringopsfinal.py
+++ stringopsfinal.py
@@ -1,7 +1,14 @@
+"""
+        Write a Python program to compute following operations on String: 
+        a) To display word with the longest length 
+        b) To determines the frequency of occurrence of particular character in the string 
+        c) To check whether given string is palindrome or not  
+        d) To display index of first appearance of the substring 
+        e) To count the occurrences of each word in a given string
+
+"""
+
 import string
-
-from cupshelpers.ppds import normalize
-
 
 def palindrome(my_string):
     for search in range(0, len(my_string), 1):
