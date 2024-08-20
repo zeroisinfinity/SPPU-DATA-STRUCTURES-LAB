5c4b6c9e20f30ece0e199ce67b58fc3b98248919
zeroisinfinity <skm842011@gmail.com>
2024-08-21 00:21:03 +0530

    Update and rename linear_search.py to Python_GroupB_1.py

--- Python_GroupB_1.py
+++ Python_GroupB_1.py
@@ -1,3 +1,10 @@
+"""a) Write a pythonprogram to store roll numbers of student in array who attended training program in random order. 
+Write function for searching whether particular student attended training program or not, 
+using Linear search and Sentinel search."""
+
+
+
+
 import string
 def menu_random():
     print(
