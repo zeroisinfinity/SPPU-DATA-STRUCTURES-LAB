e0dad67006e8c90d4b1d2a9f9a5d3e820f11d800
Sahil More <skm842011@gmail.com>
2024-09-01 10:21:03 +0530

    Update Python_GroupB_1a.py

--- Python_GroupB_1a.py
+++ Python_GroupB_1a.py
@@ -1,4 +1,4 @@
-"""a) Write a pythonprogram to store roll numbers of student in array who attended training program in random order. 
+"""a) Write a python program to store roll numbers of student in array who attended training program in random order. 
 Write function for searching whether particular student attended training program or not, 
 using Linear search and Sentinel search."""
 
