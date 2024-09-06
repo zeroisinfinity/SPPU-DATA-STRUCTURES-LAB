4aa6afe7c95c1b4b757b73ea22993337d20863ed
Sahil More <skm842011@gmail.com>
2024-09-07 01:00:31 +0530

    Update Python_Group_B_1b.py

--- Python_Group_B_1b.py
+++ Python_Group_B_1b.py
@@ -1,19 +1,16 @@
 """
-b) Write a python program to store roll numbers of student array who attended training program in sorted order. 
-Write function for searching whether particular student attended training program or not, 
+b) Write a python program to store roll numbers of student array who attended training program in sorted order.
+Write function for searching whether particular student attended training program or not,
 using Binary search and Fibonacci search
 """
 
-
-
-
-import string
 import sys
+import random
 sys.setrecursionlimit(3000)
 
 def menu():
     print("Enter 1 for Iterative Binary search.\nEnter 2 for Recursive Binary search.\nEnter 3 for left-to-right Fibonacci search\nEnter 4 for right-to-left Fibonacci search")
-menu_random()
+menu()
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -37,14 +34,30 @@ def getroll(size):
         assign += 1
     return array
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-def selecion_sort(sorted_array):
-    for compare in range(0,len(sorted_array),1):
-        for iter in range(compare+1,len(sorted_array),1):
-            if sorted_array[compare] > sorted_array[iter]:
-                sorted_array[compare],sorted_array[iter] = sorted_array[iter],sorted_array[compare]
-    return sorted_array
-
+def quicksort(array, start_pos, end_pos):
+    if start_pos < end_pos:
+        new_pivot = shuffle(array, start_pos, end_pos)
+        quicksort(array, start_pos, new_pivot)
+        quicksort(array, new_pivot + 1, end_pos)
+def shuffle(array,start_pos,end_pos):
+    sh_pivot = random.randint(start_pos,end_pos)
+    array[sh_pivot] , array[start_pos] = array[start_pos] , array[sh_pivot]
+    return partition(array,start_pos,end_pos)
+def partition(array, start_pos, end_pos):
+    pivot = array[start_pos]
+    inc = start_pos - 1
+    dec = end_pos + 1
+    while True:
+        inc += 1
+        while array[inc] < pivot:
+            inc += 1
+        dec-=1
+        while array[dec] > pivot:
+            dec-=1
+        if inc >= dec:
+            return dec
+        array[inc] , array[dec] = array[dec] , array[inc]
+        #print(array)
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def conventional_fibonacchi_search(fib_array, records, target):
@@ -167,8 +180,9 @@ def recursive_binary_search(bin_array, start_pos, end_pos, target):
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 list_of_roll = getroll(strength)
-sorted_list = selection_sort(list_of_roll)
-print(sorted_list)
+roll_list_copy = list_of_roll
+quicksort(list_of_roll,0,len(list_of_roll) - 1)
+print(list_of_roll)
 menu()
 choice = int(input("Enter choice - "))
 while choice not in (1, 2, 3, 4):
@@ -177,42 +191,44 @@ while choice not in (1, 2, 3, 4):
 check = True
 while check:
     if choice == 1:
-        search_result = iterative_binary_search(sorted_list,0,strength - 1, key)
+        search_result = iterative_binary_search(list_of_roll,0,strength - 1, key)
         if search_result == -1:
             print("Key isn't present.")
         else:
             print("Key present at", search_result+1, "location of the list of roll numbers.")
 
     elif choice == 2:
-        search_result = recursive_binary_search(sorted_list,0,strength - 1, key)
+        search_result = recursive_binary_search(list_of_roll,0,strength - 1, key)
         if search_result == -1:
             print("Key isn't present.")
         else:
             print("Key present at", search_result+1,"location of the list of roll numbers.")
 
-    elif choice == 2:
-        search_result = conventional_fibonacchi_search(sorted_list,strength, key)
+    elif choice == 3:
+        search_result = conventional_fibonacchi_search(list_of_roll,strength, key)
         if search_result == -1:
             print("Key isn't present.")
         else:
             print("Key present at", search_result+1,"location of the list of roll numbers.")
 
     else:
-        search_result = nonconventional_fibonacchi_search(sorted_list,strength,key)
+        search_result = nonconventional_fibonacchi_search(list_of_roll,strength,key)
         if search_result == -1:
             print("Key isn't present.")
         else:
             print("Key present at",search_result+1,"location of the list of roll numbers.")
 
     to_continue = input("Want to continue?\nyes or no - ")
-    while (to_continue.lower() not in ("yes", "no")):
+    to_continue = to_continue.lower()
+    while to_continue not in ("yes", "no"):
         print("ENTER ONLY YES OR NO\n--------------------------------------------------")
         to_continue = input("Want to continue?\nyes or no - ")
 
-    if to_continue.lower() == "no":
+    if to_continue == "no":
         print("--------------------------\nThnx for using our program.")
         check = False
     else:
+        menu()
         choice = int(input("Enter choice - "))
         while choice not in (1, 2, 3, 4):
             print("Pls enter appropriate choice - \n----------------------------------------------")
