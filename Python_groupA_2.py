41b0170b0983ba570fb40e577fb2c76d94d0f101
Sahil More <skm842011@gmail.com>
2024-09-07 00:21:05 +0530

    Update Python_groupA_2.py

--- Python_groupA_2.py
+++ Python_groupA_2.py
@@ -91,7 +91,6 @@ def avg(l):
 def hfreq(l):
     count_initial = 1  # initial count
     freq_list = []  # will store marks with freq > 1
-    count_dyn_list = []  # list of counts>1
     length = len(l)  # length of list
 
     if absent(l)==no_of_stud: #Verifying if all are absent
@@ -112,28 +111,22 @@ def hfreq(l):
         # Updating count_dyn_list and freq_list
         # To store equal counts corresponding to marks with those counts at a given instant
         if count_dyn == count_initial:
-            count_initial = count_dyn #DON'T FORGET TO UPDATE THE count_initial
-            count_dyn_list.append(count_dyn)
             freq_list.append(l[i])
 
         #Set the bar high if higher count_dyn is found
         elif count_dyn>count_initial:
             freq_list.clear()
-            count_dyn_list.clear()
-            count_initial = count_dyn
-            count_dyn_list.append(count_dyn)
+            count_initial = count_dyn #DON'T FORGET TO UPDATE THE count_initial
             freq_list.append(l[i])
 
     # To check if all got distinct marks
-    if (len(count_dyn_list) == no_of_stud):
+    if count_initial == 1 and len(freq_list) == no_of_stud:
         print("Every marks' frequency is 1.")
-    # If all are absent
-    elif len(count_dyn_list) == 0:
-        print("All are absent.")
-    # If None of above
+    # If not
     else:
-        for iter in range(len(count_dyn_list)):
-            print("Highest frequency marks is ", freq_list[iter], " with frequency of - ", count_dyn_list[iter])
+        for iter in range(len(freq_list)):
+            print("Highest frequency marks is ", freq_list[iter], " with frequency of - ", count_initial)
+
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Defining low function
 def low(l):
