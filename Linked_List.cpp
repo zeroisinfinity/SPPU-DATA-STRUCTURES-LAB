3dc29ae02c0e21d22ead4ccb3bef1650c8d4728d
Sahil More <skm842011@gmail.com>
2024-09-20 11:58:35 +0530

    Update Linked_List.cpp

--- Linked_List.cpp
+++ Linked_List.cpp
@@ -13,7 +13,7 @@ class LinkedList{
 			Node* Head;
 			LinkedList():Head(NULL){};
 			
-			//INSERT AT BEGINNING 
+
 			void insert_at_start(int value);
 			void insert_at_location(int value, int location);
 			void insert_at_end(int value);
@@ -54,7 +54,7 @@ void LinkedList :: insert_at_location(int value , int location){ //bracket-1
 		//arg_node -> next = Head;
 		Node* ll_holder = Head;
 		
-		//IF LINKEDLIST IS EMPTY
+
 		for(int iter = 0; iter < (location - 1 ) && ll_holder ; ++iter){//bracket-4
 				ll_holder = ll_holder -> next;
 			}//bracket-4
@@ -63,9 +63,9 @@ void LinkedList :: insert_at_location(int value , int location){ //bracket-1
 		ll_holder -> next = arg_node;
 		
                 
-        if(!ll_holder){//b-4
+        if(!ll_holder){//b-5
                 cout<<"Location out of range."<<endl;
-            }//b-4
+            }//b-5
 }//bracket-1
 
 void LinkedList :: insert_at_end(int value){//b1
@@ -86,14 +86,7 @@ void LinkedList :: insert_at_end(int value){//b1
         ll_holder -> next = arg_node;
 }//b1
 
-void LinkedList :: del_from_end(void){//b1
-		Node* ll_holder = Head;
-		
-		//IF LINKEDLIST IS EMPTY
-		while(!ll_holder){//bracket-4
-				ll_holder = ll_holder -> next;
-			}//bracket-4
-		
+
 void LinkedList :: display_LL(void){//b1
 
         if(!Head){//b2
@@ -111,7 +104,7 @@ void LinkedList :: display_LL(void){//b1
 
 void LinkedList::del_from_start(void){//b1
 		Node* ll_holder = Head -> next;
-		free(Head); 
+		delete Head; 
 		Head = ll_holder;
 		
 }//b1
@@ -125,32 +118,48 @@ void LinkedList :: del_from_location(int location){//b1
 		for(int travs = 0 ; travs < (location - 1) && ll_holder ; travs++){//b3 
 			ll_holder = ll_holder -> next;
 			}//b3
-		if(!ll_holder){//b3
+		if(!ll_holder){//b4
 			cout<<"out of range"<<endl;
-		}//b3
+		}//b4
 		Node* fake_node;
 		fake_node -> next = ll_holder -> next;
 		ll_holder = fake_node -> next;
-		//free(fake_node);
-		}//b1
+}//b1
+void LinkedList :: del_from_end(void){//b1
+		if(!Head){//b2
+			cout<<"List is empty"<<endl;
+		}//b2
+		
+		Node* ll_holder = Head;
+		while(ll_holder->next->next){//b3
+			ll_holder = ll_holder -> next;
+			}//b3
+		delete ll_holder->next;
+		ll_holder->next = NULL;
+			
+}//b1
+int main(){
 
-int main() {
-    // Initialize a new linked list
     LinkedList list1;
 
-    // Insert elements at the end
     list1.insert_at_end(10);
     list1.insert_at_end(20);
-
-    // Insert element at the beginning
+    list1.insert_at_end(11);
+    list1.insert_at_end(21);
+    list1.insert_at_end(12);
+    list1.insert_at_end(22);
+    list1.insert_at_end(13);
+    list1.insert_at_end(23);
     list1.insert_at_start(5);
-
-    // Insert element at a specific position
+	
     list1.insert_at_location(15, 3);
-	list1.del_from_start();
-	list1.del_from_location(2);
-
+	
+    list1.del_from_start();
+    list1.del_from_location(2);
+    list1.del_from_end();
+	
     cout << "Linked list after insertions: "<<endl;
+	
     list1.display_LL();
-return 0;
+	return 0;
 }
